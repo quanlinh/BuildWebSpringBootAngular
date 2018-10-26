6b9563d5027f9d123c01f8fb72ea5a778c7ccbf5
QUAN THE TRAN <qtran88@cs.washington.edu>
2018-10-26 10:09:22 -0700

    working on JPA entities

---
+++ src/main/java/com/quanthetran/spring5webappQuanTran/Spring5webappQuanTranApplication.java
@@ -0,0 +1,15 @@
+package com.quanthetran.spring5webappQuanTran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+
+@SpringBootApplication
+public class Spring5webappQuanTranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappQuanTranApplication.class, args);
+	}
+}
